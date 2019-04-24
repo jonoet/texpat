3a97bfb275192a71bceaedd97e13ea23b849b7e9
jonoet <jonoet@gmail.com>
2019-04-24 22:05:52 +0200

    labels in stack

--- texpat.cc
+++ texpat.cc
@@ -1,20 +1,26 @@
 #include <iostream>
+#include <stack>
 #include "xmlparser.h"
 using namespace std;
 
 class TestParser : public XMLParser {
-		void OnStart(const XML_Char *name, const XML_Char **atts) {
-			printf ("We got a start element %s\n", name);
-			return;
-		}
-		void OnEnd(const XML_Char *name) {
-			printf ("We got an end element %s\n", name);
-			return;
-		}
-		void OnData(const XML_Char *s, int len) {
-			printf ("We got %d bytes of data\n", len);
-			return;
-		}
+	public:
+	stack<string> label;
+	
+	void OnStart(const XML_Char *name, const XML_Char **atts) {
+		label.push(name);
+		printf ("We got a start element %s (%d)\n", name, (int)label.size());
+		return;
+	}
+	void OnEnd(const XML_Char *name) {
+		printf ("We got an end element %s (%d)\n", name, (int)label.size());
+		label.pop();
+		return;
+	}
+	void OnData(const XML_Char *s, int len) {
+		printf ("We got %d bytes of data\n", len);
+		return;
+	}
 };
 
 int main() {
